2985c141b62baea4ae1c5afa3c537c3aa44b9a4d
Jon Distad <jon.distad@gmail.com>
2011-04-28 23:43:34 -0400

    CLean up and use JFrame

--- src/advenjure/core.clj
+++ src/advenjure/core.clj
@@ -1,31 +1,33 @@
 (ns advenjure.core
-  (:use advenjure.sprite)
+  (:require [advenjure.sprite :as sprite])
   (:import [java.awt GraphicsEnvironment GraphicsDevice GraphicsConfiguration Graphics2D]
-	   [javax.swing JWindow JPanel]))
+	   [javax.swing JFrame JPanel JWindow]))
 
 (def gfx-env (GraphicsEnvironment/getLocalGraphicsEnvironment))
 (def gfx-device (.getDefaultScreenDevice gfx-env))
 (def gfx-config (.getDefaultConfiguration gfx-device))
 
-(def window
-  (let [tiles (load-tiles "resources/player.png" 2 4)
-	tile-width (.getWidth (first tiles))
-	tile-height (.getHeight (first tiles))
-	pane (proxy [JPanel] [true]
-	       (paint [gfx]
-		      (.clearRect gfx 0 0 100 100)
-		      (.drawImage gfx
-				  (nth tiles (-> (System/currentTimeMillis) (rem 2400) (quot 300)))
-				  0 0 nil)))]
-    (doto (JWindow. gfx-config) (.add pane) (.setSize 640 480))))
+(def player-tiles (sprite/load-tiles "resources/player.png" 2 4))
 
-(defn cycle-it []
-  (loop []
-    (Thread/sleep 50/3)
-    (.repaint (.getContentPane window) 1 0 0 (.getWidth window) (.getHeight window))
-    (recur)))
+(defn player-sprite []
+  (nth player-tiles
+       (-> (System/currentTimeMillis)
+	   (rem 2400)
+	   (quot 300))))
 
-(defn draw-it []
-  (.setFullScreenWindow gfx-device window)
-  (Thread/sleep 2000)
-  (.setFullScreenWindow gfx-device nil))
+(def pane
+  (proxy [JPanel] []
+    (paint [gfx]
+	   (.drawImage gfx (player-sprite) 0 0 nil))))
+
+(def frame (JFrame.))
+(def window (JWindow.))
+
+(doto frame (.add pane) (.setSize 640 480) (.setVisible true))
+#_(doto window (.add pane))
+#_(.setFullScreenWindow gfx-device window)
+
+(loop []
+  (Thread/sleep 1000/60)
+  (.repaint pane)
+  (recur))
