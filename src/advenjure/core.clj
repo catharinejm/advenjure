6f15c1fb7076acd520e29c27673ec7d3f4edee92
Jon Distad <jon.distad@gmail.com>
2011-04-28 21:00:29 -0400

    animation test

--- src/advenjure/core.clj
+++ src/advenjure/core.clj
@@ -9,12 +9,21 @@
 
 (def window
   (let [tiles (load-tiles "resources/player.png" 2 4)
+	tile-width (.getWidth (first tiles))
+	tile-height (.getHeight (first tiles))
 	pane (proxy [JPanel] [true]
 	       (paint [gfx]
+		      (.clearRect gfx 0 0 100 100)
 		      (.drawImage gfx
 				  (nth tiles (-> (System/currentTimeMillis) (rem 2400) (quot 300)))
 				  0 0 nil)))]
     (doto (JWindow. gfx-config) (.add pane) (.setSize 640 480))))
+
+(defn cycle-it []
+  (loop []
+    (Thread/sleep 50/3)
+    (.repaint (.getContentPane window) 1 0 0 (.getWidth window) (.getHeight window))
+    (recur)))
 
 (defn draw-it []
   (.setFullScreenWindow gfx-device window)
