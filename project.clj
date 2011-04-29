e8b742475f3b778043723fdb65a1a590f1544476
Jon Distad <jon.distad@gmail.com>
2011-04-28 21:00:38 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject advenjure "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.3.0"]])
